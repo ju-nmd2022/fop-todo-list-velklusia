9cf43774e1dad9e9743c3c628c598052911e9428
velklusia <116886960+velklusia@users.noreply.github.com>
2023-04-11 23:56:58 +0200

    Loaded tasks from local storage on page load

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ window.addEventListener('load', ()=>{
     const input= document.querySelector("#task-input");
     const list= document.querySelector("#tasks");
     let tasks = [];
+
+    if (localStorage.getItem('tasks')) {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.forEach(task => {
+            addTaskToList(task);
+        });
+    }
