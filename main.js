b9325e662372dc6ce5e74416539ef455d5ec9ff5
velklusia <116886960+velklusia@users.noreply.github.com>
2023-04-11 23:56:09 +0200

    Initialized tasks

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', ()=>{
+    const form= document.querySelector("#task-form");
+    const input= document.querySelector("#task-input");
+    const list= document.querySelector("#tasks");
+    let tasks = [];
